1d9fc7eb157d255ec8220a981d1d1a866ce6b326
Kavya-P-1016 <83158328+Kavya-P-1016@users.noreply.github.com>
2026-03-01 00:36:43 +0530

    Create app.tsx

---
+++ src/app.tsx
@@ -0,0 +1,10 @@
+/**
+ * @license
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import F1GlobeVisualization from './components/F1GlobeVisualization';
+
+export default function App() {
+  return <F1GlobeVisualization />;
+}
